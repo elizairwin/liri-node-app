8e8bceafc4fac9759742c225790e3a6a6d3fe7ab
Eliza Irwin <elizairwin@gmail.com>
2019-08-17 12:59:27 -0400

    working on js file

--- liri.js
+++ liri.js
@@ -1,4 +1,4 @@
-//add the npm pacakages installed
+//add the required libraries/pacakages installed
 require("dotenv").config();
 const axios = require("axios");
 const fs = require("fs");
@@ -13,6 +13,11 @@ let spotify = new Spotify(keys.spotify);
 //define variables using process.argv below
 let command = process.argv[2]
 let searchTerm = process.argv[3]
+
+//error
+fs.appendFile('log.txt', command + ",", function (err) {
+    if (err) throw err;
+});
 
 //switch command since there are three scenarios here
 switch (command){
@@ -50,15 +55,53 @@ inquirer.prompt([
     //the album that the song is from
 //if no song is provided then choose a default song to submit to the API
 
+if (response.choices === ("spotify-this-song")) {
+
 inquirer.prompt([
     {
     type: "input",
     message: "What song do you want to hear?",
-    mame: "song"
+    mame: "track"
     },
+
+    function spotify(song) {
+        spotify
+        .search({ type: "track", query: song })
+        .then(function(response){
+            if (response.tracks.total === 0) {
+                errorConditionForSpotify();
+            } else {
+                console.log("Artist: " + response.tracks.items[0].artists[0].name);
+                console.log("Track: " + response.tracks.items[0].name);
+                console.log("Preview URL: " + response.tracks.items[0].preview_url);
+                console.log("Album: " + response.tracks.items[0].album.name);
+            }
+        }).catch(function (error) {  
+            console.log(error);
+            console.log("No Results found. Showing results for 'Oops!....I Did It Again' by Brittany Spears");
+      });
+    },
+
+    function errorConditionForSpotify() {
+        spotify
+        .search({ type: 'track', query: 'Oops!...I Did It Again' })
+        .then(function(response) {
+            for (var i=0;i < response.tracks.items.length; i++) {
+                if (response.tracks.items[i].artists[0].name === "Brittany Spears") {
+                    console.log("Artist: " + response.tracks.items[i].artists[0].name);
+                    console.log("Track: " + response.tracks.items[i].name);
+                    console.log("Preview URL: " + response.tracks.items[i].preview_url);
+                    console.log("Album: " + response.tracks.items[i].album.name);
+                    i = response.tracks.items.length;
+                }
+            }
+        }).catch(function (error) {  
+            console.log(error);
+            console.log("No Results found. ");
+      });
+    }
 ])
 
-if (response.choices === ("spotify-this-song") {
 //movie-this
 //this will show in the terminal:
     //title of the movie.
@@ -71,21 +114,51 @@ if (response.choices === ("spotify-this-song") {
     //actors in the movie.
 //if the user doesn't type a movie in, then choose a default movie to submit to the API
 
+if (response.choices === ("spotify-this-song")) {
+
 inquirer.prompt([
     {
     type: "input",
     message: "What movie do you want to know about?",
-    mame: "movie"
+    name: "movie"
     },
-])
+
+    function movieThis(movie) {
+        axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
+            function(response) {
+                //console.log(response.data);
+                if (response.data.Title != undefined) {
+                    console.log("Title: " + response.data.Title);
+                    console.log("Year: " + response.data.Year);
+                    console.log("imdbRating:: " + response.data.imdbRating);
+                    console.log("Title: " + response.data.Title);
+                    console.log("Country:: " + response.data.Country);
+                    console.log("Language:: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot);
+                    console.log("Actors: " + response.data.Actors);
+                    console.log("RottenTomatoes: " + response.data.tomatoRating);
+                } 
+                else {
+                    movieThis("Mr. Nobody");
+                }
+            }
+
+        ).catch(function (error) {  
+            console.log(error);
+            console.log("No Results found. ");
+      });
+    },
 
 //do-what-it-says
 //will take text in random.text and use it to call one of Liri's commands
 //edit the text in random.txt to test out the feature for movie-this
 
-.prompt([
-    {
-    
-    }
-    
-    ])
+// function doRandom() {
+//     fs.readFile("random.txt", "utf8", function(error, data) {
+//         var dataArr = data.split(",");
+//         spotify(dataArr[1])
+//         if (error) {
+//           return console.log(error);
+//         }
+//     });
+// }
